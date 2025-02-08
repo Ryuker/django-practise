a9787b3c0b66d2506ebb657fe5120cb5349ae599
ryuker <lifeisagamenotaproduct@gmail.com>
2025-02-08 16:23:57 +0100

    updated settings, added env file support

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p0b#&)5&8b#e1kj_4+tw5ej&01vzp#sjplimfr9c6r!5-f+se7'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
